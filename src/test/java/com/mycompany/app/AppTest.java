0aade5f905849a8c72614143e5f6d05b8503b10c
Venkata Bhaskar <66554216+javaexpresschannel@users.noreply.github.com>
2020-07-18 14:47:46 +0530

    duplicating same test case with different method

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -17,4 +17,35 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+    public void shouldAnswerWithTrue2()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+    public void shouldAnswerWithTrue3()
+    {
+        assertTrue( true );
+    }
+    
+    
+    @Test
+    public void shouldAnswerWithTrue4()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+    public void shouldAnswerWithTrue5()
+    {
+        assertTrue( true );
+    }
 }
